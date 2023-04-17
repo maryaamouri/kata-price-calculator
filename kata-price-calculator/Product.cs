89a71745190c3ff7eec5ea035f5ae55a7e4c15a5
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-18 02:40:33 +0300

    implement Product class

---
+++ kata-price-calculator/Product.cs
@@ -0,0 +1,13 @@
+﻿internal class Product
+{
+    public Product(string Name, int Upc, float Price)
+    {
+        this.Name = Name;
+        this.Upc = Upc;
+        this.Price = Price;
+    }
+
+    public  string Name { get;  set; }
+    public  int Upc { get;  set; }
+    public  float Price { get;  set; }
+}
