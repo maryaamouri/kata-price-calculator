90b57c974279b961f9959f126fb598a25ff7cf8a
Marya_Amouri <m.amoori@student.aaup.edu>
2023-04-18 03:05:28 +0300

    adding tax property

--- kata-price-calculator/Product.cs
+++ kata-price-calculator/Product.cs
@@ -5,9 +5,15 @@
         this.Name = Name;
         this.Upc = Upc;
         this.Price = Price;
+        Tax = (float)0.2;
     }
 
     public  string Name { get;  set; }
     public  int Upc { get;  set; }
     public  float Price { get;  set; }
+    public static float  Tax { private set; get; } 
+    private float AddTax(float Price)
+    {
+        return (float)(Price +(Price /Tax));
+    }
 }
