2da57f6fbcce9d8b9e4d681ebf1e094b5f5963ec
Marya_Amouri <marya.ammori@gmail.com>
2023-05-09 12:40:22 +0300

    remove white spaces from report class

--- kata-price-calculator/Report.cs
+++ kata-price-calculator/Report.cs
@@ -8,9 +8,6 @@
         {
             this.product = product;
         }
-
- 
-
         public void DisplayWithDiscount()
         {
             Console.WriteLine($"Tax = {Product.Tax}%, discount = {Product.Discount}%\r\nProgram prints price {product.AppllyDiscount()}\r\nProgram displays {product.GetAmountDeduced()} amount which was deduced");
