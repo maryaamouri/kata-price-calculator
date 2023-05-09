76c1f646eb43f27d7aeaaa3b6b571ffcd78f8444
Marya_Amouri <marya.ammori@gmail.com>
2023-05-09 12:39:08 +0300

    remove white space from main

--- kata-price-calculator/Program.cs
+++ kata-price-calculator/Program.cs
@@ -5,17 +5,12 @@ using System.Security.Cryptography;
 
 internal class Program
 {
-
-
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello Kata!");
         Product product1 = new Product("The Little Prince", 12345, 20.25);
         Report report1 = new Report(product1);
         report1.DisplayWithDiscount();
-        Console.WriteLine();
-        report1.DisplayWithoutDiscount();
-        Console.WriteLine();
 
 
     }
