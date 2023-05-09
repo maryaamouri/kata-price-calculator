b8709d67b2ddbbf3f709d63e0aa7bbd7c23109b7
Marya_Amouri <marya.ammori@gmail.com>
2023-05-09 10:48:23 +0300

    add test case2 when tax 21%

--- kata-price-calculator/Program.cs
+++ kata-price-calculator/Program.cs
@@ -8,7 +8,12 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello Kata!");
+        Console.WriteLine("test case1: tax 20%");
         Product product1 = new Product("The Little Prince", 12345, 20.25);
         Console.WriteLine($"the price with tax is = {product1.Price.ToString()}");
+        Console.WriteLine("test case2: tax 21%");
+        Product.Tax = 0.21;
+        Console.WriteLine($"the price with tax is = {product1.Price.ToString()}");
+
     }
 }
