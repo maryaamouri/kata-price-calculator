053e6a81e08054cbd0927e0c13111cb793cf71ce
Marya_Amouri <marya.ammori@gmail.com>
2023-05-11 19:41:00 +0300

    add test cases to the main

--- kata-price-calculator/Report.cs
+++ kata-price-calculator/Report.cs
@@ -1,4 +1,6 @@
-﻿namespace kata_price_calculator
+﻿using System.Xml.Schema;
+
+namespace kata_price_calculator
 {
     internal class Report
     {
@@ -47,22 +49,14 @@
                 product.Packaging != null)
                 addtionalCostsReport = "no addtional costs";
 
-            string productInfo = $"Title = {product.Name}, UPC = {product.Upc}";
-            string taxReport = $"Tax = {Product.Tax*100}%, ";
-            string uniDiscountReport = $"universal discount (after tax) = {_universalDiscount}%, ";
-            string upcDiscountReport = $"UPC-discount (before tax) = {_upcDiscount}% for UPC={product.Upc}";
-            string priceReport = $"UPC discount amount = ${_upcDiscountAmount}," +
-                $" remaining price = {_remainingPrice}\n" +
-                $"Tax amount = {_taxAmount}" +
-                $" universal discount amount= {_universalDiscountAmount}\n" +
-                $"Final price = {_finalPrice}";
+            _finalPrice = Math.Round(_cost + _taxAmount - _discounts+_addtionalCosts, 2);
 
             Console.WriteLine(
                 productInfo+"\n" + 
-                taxReport+
-                uniDiscountReport+"\n"+
-                upcDiscountReport + "\n" +
-                priceReport);
+                taxReport+"\n"+
+                discountReport+"\n"+
+                addtionalCostsReport+
+                $"TOTAL = {_finalPrice}" );
         }
     }
 }
