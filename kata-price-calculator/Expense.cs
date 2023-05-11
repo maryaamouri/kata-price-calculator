da79d8302787dd14cf99fefb04e981ca4c64677c
Marya_Amouri <marya.ammori@gmail.com>
2023-05-11 11:15:34 +0300

    add expenses class

---
+++ kata-price-calculator/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_price_calculator
+{
+    internal class Expense
+    {
+        string Description { set; get; }
+        double Amount { set; get; }
+
+        public Expense(string description, double amount)
+        {
+            Description = description;
+            Amount = amount;
+        }
+    }
+}
