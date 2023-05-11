833ab520cdaf36999384c2513e80832d3d6171d8
Marya_Amouri <marya.ammori@gmail.com>
2023-05-11 21:25:42 +0300

    split report into small reports

--- kata-price-calculator/Report.cs
+++ kata-price-calculator/Report.cs
@@ -2,37 +2,60 @@
 
 namespace kata_price_calculator
 {
+     
     internal class Report
     {
         Product product;
-
+        int _roundationDigits =2;
         public Report(Product product)
         {
             this.product = product;
         }
-
         public void Dispaly()
         {
             var _cost = product.Price;
-            var _taxAmount = Math.Round(product.TaxAmount(_cost), 2);
-            var _upcDiscountAmount = Math.Round(product.UpcDiscountAmount(_cost), 2);
-            var _universalDiscountAmount = Math.Round(product.UniversalDiscountAmount(_cost), 2);
-            var _discounts = _universalDiscountAmount + _upcDiscountAmount;
-            var _addtionalCosts =0.0;
+            var _finalPrice = Math.Round(product.CalculatePrice(), _roundationDigits);
 
-            var productInfo = $"Title = {product.Name}, UPC = {product.Upc}, Price = {product.Price}";
+            Console.WriteLine(
+                GenerateProductInfoReport() + "\n" +
+                GenerateTaxReport(_cost) + "\n" +
+                GenerateDiscountReport(_cost) + "\n" +
+                GenerateExpensesReport () +
+                $"TOTAL = {_finalPrice}");
+        }
+        private string GenerateProductInfoReport()
+        {
+            var productInfo = $"Title = {product.Name}," +
+                $" UPC = {product.Upc}," +
+                $" Price = {product.Price}";
+            return productInfo ;
+        }
+        private string GenerateTaxReport(double _cost)
+        {
+            var _taxAmount = Math.Round(product.TaxAmount(_cost), _roundationDigits);
             var taxReport = $"Tax = {_taxAmount}";
-            var discountReport ="";
-            var addtionalCostsReport ="";
-
+            return taxReport;
+        }
+        private string GenerateDiscountReport(double _cost)
+        {
+            var _upcDiscountAmount = Math.Round(product.UpcDiscountAmount(_cost), _roundationDigits);
+            var _universalDiscountAmount = Math.Round(product.UniversalDiscountAmount(_cost), _roundationDigits);
+            var _discounts = _universalDiscountAmount + _upcDiscountAmount;
+            var discountReport = "";
+            
             if (!product._isUpcDiscountAppllied && !product.IsUniversalDiscountAppllied)
                 discountReport += "no discounts";
-            else  discountReport = $"discount = {_discounts}";
-
-            if(product.Transport != null)
+            else discountReport = $"discount = {_discounts}";
+            return discountReport;
+        }
+        private string GenerateExpensesReport()
+        {
+            var addtionalCostsReport = "";
+            var _addtionalCosts = 0.0;
+            if (product.Transport != null)
             {
                 addtionalCostsReport += $"Transport = {product.Transport.Amount} \n";
-                _addtionalCosts = _addtionalCosts+product.Transport.Amount;
+                _addtionalCosts = _addtionalCosts + product.Transport.Amount;
             }
             if (product.Administrative != null)
             {
@@ -44,19 +67,13 @@ namespace kata_price_calculator
                 addtionalCostsReport += $"Packaging = {product.Packaging.Amount}\n";
                 _addtionalCosts += product.Packaging.Amount;
             }
-            else if(product.Transport == null &&
-                product.Administrative != null&&
+            else if (product.Transport == null &&
+                product.Administrative != null &&
                 product.Packaging != null)
                 addtionalCostsReport = "no addtional costs";
 
-            var _finalPrice = Math.Round(_cost + _taxAmount - _discounts+_addtionalCosts, 2);
-
-            Console.WriteLine(
-                productInfo+"\n" + 
-                taxReport+"\n"+
-                discountReport+"\n"+
-                addtionalCostsReport+
-                $"TOTAL = {_finalPrice}" );
+            return addtionalCostsReport;
         }
+
     }
 }
